2a75b1de9f7bd940618ea52d7907685257f7fc2e
AnnevanGorkom <anne@vangorkom.com>
2020-10-15 15:47:16 +0200

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
