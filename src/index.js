49d1da54ce01ad9a402e777bbaee8e94f6a5c5da
AnnevanGorkom <anne@vangorkom.com>
2020-11-01 13:09:26 +0100

    Removed unused index.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
